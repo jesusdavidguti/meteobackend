f2f21ac79adc358b519ede1a641555fc0975c0dd
usuario <jesusdavid.guti@gmail.com>
2021-01-24 21:04:22 +0100

    Primer commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // --> lee las variables de entorno del archivo .env y las
+                            // incorpora a process.env.PORT
+const path       = require('path');
+const cors       = require('cors');
+const express    = require('express');
+const mongoose   = require('mongoose'); // Es un paquete
+const apiRoutes  = require('./routes'); // Es un archivo
+
+const app    = express();
+const PORT   = process.env.PORT || 3000; // "process" es un objeto que contiene información acerca
+                                         // de la aplicación. No es necesario importarlo, pero se
+                                         // podría. Contiene muchas propiedades y métodos.
+const DB_URI = process.env.DB_URI;
+
+// CONEXIÓN A BASE DE DATOS
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => console.log("Conexión a BD de meteoBackend correcta"))
+    .catch(error => console.log("Error al conectarse a la BD de metoBackend" + error));
+
+
+// MIDDLEWARE
+app.use(cors());            // Soporte para CORS
+app.use(express.json());    // IMPORTANTE: Poner esto antes de las rutas
+app.use('/api', apiRoutes);
+app.use(express.static(path.join(__dirname , 'public')));
+
+// SERVIDOR WEB
+app.listen(PORT, () => console.log("Servidor iniciado para meteoBackend..."));
