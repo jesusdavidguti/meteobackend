bf7b522bda2a99d99159cfad3a3cd83688f73c3f
usuario <jesusdavid.guti@gmail.com>
2021-01-27 08:12:54 +0100

    Avisos

--- routes.js
+++ routes.js
@@ -8,17 +8,10 @@ const router = express.Router();
 
 // --------------- API REST CRUD
 
-router.get    ("/clientes",      cors(), controller.readClientes);   // Read All
-router.get    ("/clientes/:id",  cors(), controller.readCliente);    // Read
-router.delete ("/clientes/:id",  cors(), controller.deleteCliente);  // Delete
-router.put    ("/clientes/:id",  cors(), controller.updateCliente);  // Update
-router.post   ("/clientes",      cors(), controller.createCliente);  // Create
-
-router.get    ("/articulos",     cors(), controller.readArticulos);  // Read All
-router.get    ("/articulos/:id", cors(), controller.readArticulo);   // Read
-router.delete ("/articulos/:id", cors(), controller.deleteArticulo); // Delete
-router.put    ("/articulos/:id", cors(), controller.updateArticulo); // Update
-router.post   ("/articulos",     cors(), controller.createArticulo); // Create
-
+router.get    ("/avisos",      cors(), controller.readAvisos);  // Read All
+router.get    ("/avisos/:id",  cors(), controller.readAviso);   // Read
+router.delete ("/avisos/:id",  cors(), controller.deleteAviso); // Delete
+router.put    ("/avisos/:id",  cors(), controller.updateAviso); // Update
+router.post   ("/avisos",      cors(), controller.createAviso); // Create
 
 module.exports = router;
