8af8eae54fc090793fc5d885e02279e7714803a1
usuario <jesusdavid.guti@gmail.com>
2021-01-28 06:39:57 +0100

    Subida

--- routes.js
+++ routes.js
@@ -14,4 +14,17 @@ router.delete ("/avisos/:id",  cors(), controller.deleteAviso); // Delete
 router.put    ("/avisos/:id",  cors(), controller.updateAviso); // Update
 router.post   ("/avisos",      cors(), controller.createAviso); // Create
 
+router.get    ("/fenomenos",     cors(), controller.readFenomenos);  // Read All
+router.get    ("/fenomenos/:id", cors(), controller.readFenomeno);   // Read
+router.delete ("/fenomenos/:id", cors(), controller.deleteFenomeno); // Delete
+router.put    ("/fenomenos/:id", cors(), controller.updateFenomeno); // Update
+router.post   ("/fenomenos",     cors(), controller.createFenomeno); // Create
+
+router.get    ("/previsiones",      cors(), controller.readPrevisiones);    // Read All
+router.get    ("/previsiones/:id",  cors(), controller.readPrevision);      // Read
+router.delete ("/previsiones/:id",  cors(), controller.deletePrevision);    // Delete
+router.put    ("/previsiones/:id",  cors(), controller.updatePrevision);    // Update
+router.post   ("/previsiones",      cors(), controller.createPrevision);    // Create
+
+
 module.exports = router;
